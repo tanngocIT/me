44fb19b7f81812561563ba95909582f72d96b07e
Tan Ngoc <tanngocIT@gmail.com>
2021-08-01 21:16:07 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,253 @@
+import { Fragment, useRef } from 'react';
+import { FaGlobe, FaGraduationCap, FaThumbsUp, FaGrav } from 'react-icons/fa';
+import ReactPDF from 'react-to-pdf';
+import {
+	INFO,
+	EDUCATION,
+	SKILLS,
+	LANGUAGES,
+	INTERESTS,
+	EXPERIENCES,
+} from './constants';
+
+function App() {
+	const ref = useRef();
+	const personalInfo = () => {
+		return (
+			<section className='block border-box'>
+				<h3>Personal Info</h3>
+				<ul className='mt-3 info'>
+					<li>
+						<span>Birthdate</span> : {INFO.BIRTHDAY}
+					</li>
+					<li>
+						<span>Phone</span> : {INFO.PHONE}
+					</li>
+					<li>
+						<span>Email</span> : <a href = {"mailto:" + INFO.EMAIL}>{INFO.EMAIL}</a>
+					</li>
+					<li>
+						<span>Skype</span> : {INFO.SKYPE}
+					</li>
+					<li>
+						<span>Linkedin</span> : <a href={INFO.LINKEDIN_URL} target="_blank">{INFO.LINKEDIN_TITLE}</a>
+					</li>
+					<li>
+						<span>Freelance</span> :{' '}
+						{INFO.FREELANCER ? 'Available' : 'Not Available'}
+					</li>
+					<li>
+						<span>Address</span> : {INFO.ADDRESS}
+					</li>
+				</ul>
+			</section>
+		);
+	};
+
+	const education = () => {
+		return (
+			<section className='block border-box'>
+				<h3>Education</h3>
+				<div className='block-icon'>
+					<FaGraduationCap />
+				</div>
+
+				<div className='timeline mt-3'>
+					{EDUCATION.map((edu, idx) => (
+						<div key={idx} className='time'>
+							<div className='hgroup mb-3'>
+								<h4>{edu.title}</h4>
+								<h6>{edu.time}</h6>
+							</div>
+							<p>{edu.description}</p>
+						</div>
+					))}
+				</div>
+			</section>
+		);
+	};
+
+	const skills = () => {
+		return (
+			<section className='block border-box'>
+				<h3>Skills</h3>
+				<div className='block-icon'>
+					<FaGrav />
+				</div>
+
+				<div className='skills mt-3'>
+					<ul>
+						{Object.keys(SKILLS).map((key) => {
+							const value = SKILLS[key];
+							return (
+								<li key={key}>
+									<h4>{key}</h4>
+									<div className='rating'>
+										{[...Array(5).keys()].map(
+											(key, index) => {
+												return (
+													<span
+														key={key}
+														className={
+															index + 1 > value
+																? 'transparent'
+																: ''
+														}
+													></span>
+												);
+											}
+										)}
+									</div>
+								</li>
+							);
+						})}
+					</ul>
+				</div>
+			</section>
+		);
+	};
+
+	const languages = () => {
+		return (
+			<section className='block border-box'>
+				<h3>Languages</h3>
+				<div className='block-icon'>
+					<FaGlobe />
+				</div>
+
+				<div className='skills mt-3'>
+					<ul>
+						{Object.keys(LANGUAGES).map((key) => {
+							const value = LANGUAGES[key];
+							return (
+								<li key={key}>
+									<h4>{key}</h4>
+									<div className='rating'>
+										{[...Array(5).keys()].map(
+											(key, index) => {
+												return (
+													<span
+														key={key}
+														className={
+															index + 1 > value
+																? 'transparent'
+																: ''
+														}
+													></span>
+												);
+											}
+										)}
+									</div>
+								</li>
+							);
+						})}
+					</ul>
+				</div>
+			</section>
+		);
+	};
+
+	const interests = () => {
+		return (
+			<section className='block border-box rm-b'>
+				<h3>Interests</h3>
+				<div className='block-icon'>
+					<FaThumbsUp />
+				</div>
+				<div className='hobbies mt-4'>
+					<ul>
+						{INTERESTS.map((i, idx) => (
+							<Fragment key={idx}>
+								<li>
+									<span>{i.icon}</span>
+									<h6>{i.label}</h6>
+								</li>
+							</Fragment>
+						))}
+					</ul>
+				</div>
+			</section>
+		);
+	};
+
+	const summary = () => {
+		return (
+			<section>
+				<div className='block border-box'>
+					<h3>Summary</h3>
+					<p className='mt-3 summary'>
+						I'm a software engineer with more than 4 years of experience in Web Development. 
+						I'm a creative, hardworking, enthusiastic,
+						ambitious and able to work under high pressure.<br></br>
+						 My focus in on trying to
+						learn as much as possible and doing my best in order to
+						accomplish assigned task, supporting my team members,
+						research and apply new technologies effectively.
+						I'm passionate about new technology and
+						professional work environment. And I'm so funny 🙂
+					</p>
+				</div>
+			</section>
+		);
+	};
+
+	const workExperiences = () => {
+		return (
+			<section className='block sticky'>
+				<h3>Work Experiences</h3>
+				<div className='work-experiences mt-4'>
+					{EXPERIENCES.map((exp, idx) => (
+						<div key={idx} class='listing-content'>
+							<h6 className='exp-date'>{exp.date}</h6>
+							<h6 className='exp-title'>{exp.title}</h6>
+							<p className='tag'>{exp.responsibility}</p>
+							<p className='mt-3'>{exp.description}</p>
+							<div>
+								{exp.techs.map((t, ind) => (
+									<span className='tag' key={ind}>
+										{t}
+									</span>
+								))}
+							</div>
+						</div>
+					))}
+				</div>
+			</section>
+		);
+	};
+
+	return (
+		<>
+			<ReactPDF>
+				{({ toPdf, targetRef }) => (
+					<div className='main' ref={targetRef}>
+						<header className='header'>
+							<div className='name'>
+								<h2>Tan Nguyen</h2>
+								<span>Senior Software Engineer</span>
+							</div>
+							<div className='avatar'></div>
+						</header>
+
+						<main className='d-flex'>
+							<div className='left f-1'>
+								{personalInfo()}
+								{education()}
+								{skills()}
+								{languages()}
+								{interests()}
+							</div>
+
+							<div className='right f-2'>
+								{summary()}
+								{workExperiences()}
+							</div>
+						</main>
+					</div>
+				)}
+			</ReactPDF>
+		</>
+	);
+}
+
+export default App;
---
+++ src/constants/index.js
@@ -0,0 +1,146 @@
+import { FaMusic, FaGamepad, FaPlane, FaCookie } from "react-icons/fa";
+
+export const INFO = {
+	BIRTHDAY: "16-09-1995",
+	EMAIL: "tanngocit@gmail.com",
+	SKYPE: "tanngocit",
+	PHONE: "0773550028",
+	FREELANCER: true,
+	LINKEDIN_URL:"https://www.linkedin.com/in/tanngocit",
+	LINKEDIN_TITLE:"tanngocit",
+	ADDRESS: "Da Nang, Viet Nam",
+};
+
+export const EDUCATION = [
+	{
+		title: "Da Nang College of Technology - Da Nang University",
+		time: "Aug 2013 – Jun 2016",
+		description: "Bachelor of Engineering",
+	},
+];
+
+export const SKILLS = {
+	"C#, ASP.NET, .NET Core": 4,
+	NodeJs: 3,
+	"MS SQL Server, MySQL": 4,
+	React: 4,
+	"HTML/CSS": 4,
+	JQuery: 4,
+	Angular: 2, 
+	Flutter: 2,
+	Teamwork: 5,
+	Git: 5,
+};
+
+export const LANGUAGES = {
+	Vietnamese: 5,
+	English: 3,
+};
+
+export const INTERESTS = [
+	{
+		label: "Music",
+		icon: <FaMusic />,
+	},
+	{
+		label: "Game",
+		icon: <FaGamepad />,
+	},
+	{
+		label: "Travel",
+		icon: <FaPlane />,
+	},
+	{
+		label: "Cooking",
+		icon: <FaCookie />,
+	},
+];
+
+export const EXPERIENCES = [
+	{
+		title: "KYC (Samurai Security - Japan) - Orient Software",
+		date: "Jun 2021 - Present",
+		description:
+			"Coming from a company of Japan, They want to create a system to manage finacial assets, will etc on blockchain environment. " + 
+			"We are to create frontend system to KYC authentication to verify the login to secure environment.",
+		techs: ["ReactJS", "Redux"],
+		responsibility: "Team Leader",
+	},
+	{
+		title: "Plenti (Australia) - Orient Software",
+		date: "Jan 2020 - Present",
+		description:
+			"Plenti is a fintech lender, providing Australians with faster, fairer loans through smart technology." +
+			"Our proprietary technology platform allows us to provide creditworthy customers - whose preferences are changing - with automotive, " + 
+			"renewable energy and personal loans to help bring their big ideas to life. Since establishment in 2014, our loan originations have grown consistently, "+ 
+			"supported by diversified loan products, distribution channels and funding, and underpinned by exceptional credit performance and continual innovation. " + 
+			"Plenti listed on the ASX in September 2020.",
+		techs: [
+			"React", 
+			"Redux",
+			"GraphQL",
+			"S3 Bucket",
+			"Azure DevOps",
+			"MySQL",
+			"Redis",
+			".NET Core",
+			"Angular"
+		],
+		responsibility: "Senior Software Engineer",
+	},	
+	{
+		title: "Holtel Service (Vingroup) - Orient Software",
+		date: "May 2019 - Dec 2020",
+		description:
+			"Coming from Vingroup company, they want to build a part the internal service to finding hotel for comparing the price and services. " +
+			"Provide for their customer a service to search the holtels and booking. " +
+			"The system was built on web app using Angular 8.",
+		techs: ["Angular", "Azure DevOps", ".NET Core", "Kafka", "MySQL"],
+		responsibility: "Software Engineer",
+	},
+	{
+		title: "Airmine (Australia) - Orient Software",
+		date: "Aug 2018 - Jun 2019",
+		description:
+			"Airmine Project is the responsive for both web and mobile application using .NET core and React Native to manage healthcare information and display the weather using current location. " +
+			"Mobile application allows users to send the current location with temparature to the system, tracking and manage the data to report. " +
+			"The web app is an admin site for the administrator to get the detailed, and overview of the data.",
+		techs: ["React", "React Native", ".NET Core", "Docker", "SQL Server", "Blazor"],
+		responsibility: "Software Engineer",
+	},
+	{
+		title: "eNotary Video Conference - Pavaso (USA) - FPT Software",
+		date: "Jan 2018 - Aug 2018",
+		description:
+			"The project allows users to buy and sell real estate online without having to meet in person. " 
+			+ "During the transaction, the user can chat or video call and it is saved as a confirmation of the purchase and sale with a digital signature.",
+		techs: ["MS SQL Service 2012", "PowerBi"],
+		responsibility: "Software Engineer",
+	},
+	{
+		title: "Pavaso Reporting (USA) - FPT Software",
+		date: "Dec 2017 - Jan 2018",
+		description:
+			"Pavaso is a platform in real estate. This project helps the admin to understand the operating status of the platform." + 
+			"Create more reports from SQL query and visualize it by Power BI.",
+		techs: ["MS SQL Service 2012", "PowerBi"],
+		responsibility: "Software Engineer",
+	},
+	{
+		title: "SSCHJ (Japan) - FPT Software",
+		date: "Mar 2017 - Nov 2017",
+		description:
+			"The outsource project for Japanese customer. they want to update new UI for whole page in their system.",
+		techs: ["Bootstrap, Ajax/jQuery", "Cobol"],
+		responsibility: "Software Engineer",
+	},
+	{
+		title: "FINCO (Japan) - FPT Software",
+		date: "Aug 2016 - Mar 2017",
+		description:
+			"The outsource project for Japanese customer. They want to build a system that allow the user transfers money to different bank users.",
+		techs: ["Bootstrap, Ajax/jQuery", "Cobol"],
+		responsibility: "Software Engineer",
+	},
+	 
+];
